787a50a50eb0171952dfb051826497679280c28e
Zenil Haria <zenilharia06@gmail.com>
2021-03-30 18:59:32 +0530

    Added routing for signup and login.

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { LoginComponent } from './authentication/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AuthenticationComponent,
+    SignupComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
