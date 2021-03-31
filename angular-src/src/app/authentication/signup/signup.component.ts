3b2cad25695a0302e890b4bb6d053ad4e8cb3a7f
Zenil Haria <zenilharia06@gmail.com>
2021-03-31 21:48:59 +0530

    Added frontend for signup form.

--- angular-src/src/app/authentication/signup/signup.component.ts
+++ angular-src/src/app/authentication/signup/signup.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  isHospital: boolean = true;
+  registeringEntity: string = 'hospital';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggle(clickedEntity: string) {
+    this.isHospital = clickedEntity === 'hospital';
+  }
+
+  setRegisteringEntity(entity: string) {
+    this.registeringEntity = entity;
+  }
 }
