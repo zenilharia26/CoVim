299c9b2b992c86e23455a7f085307f6f35462032
Zenil Haria <zenilharia06@gmail.com>
2021-04-20 00:56:40 +0530

    Vaccine Management component added.
    The basic component for Vaccine Management is added and routing for dashboard is added. Further, the whole logic for vaccine management has to be added.

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthenticationGuard } from './authentication/authentication.guard';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'authenticate', loadChildren: () => import('./authentication/authentication-routing.module').then(module => module.AuthenticationRouting) },
-  { path: 'dashboard', component: DashboardComponent, /*canActivate: [AuthenticationGuard]*/ }
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard-routing.module').then(module => module.DashboardRouting) }
 ];
 
 @NgModule({
---
+++ angular-src/src/app/dashboard/vaccine-management/vaccine-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vaccine-management',
+  templateUrl: './vaccine-management.component.html',
+  styleUrls: ['./vaccine-management.component.css']
+})
+export class VaccineManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
